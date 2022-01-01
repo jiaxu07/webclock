4340c12df6f69ada630cf65dafc22e3824f5ff65
Joseph Zhang <tweejjhi4016@163.com>
2022-01-01 11:06:44 +0800

    index.php

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+/***
+    The Source Code of WEB CLOCK
+    (C) 2021-2022 Joseph Z.
+    All rights reserved.
+    license: BSD 3-Clause 
+***/
+
+/*
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+    AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+    IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+    FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+    DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+    SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+    CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+    OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+?>
+
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Webclock</title>
+    <script src="js/jquery-3.4.1.min.js" type="text/javascript"></script>
+    <script src="js/webclock.js" type="text/javascript"></script>
+    <link rel="stylesheet" href="css/main.css" type="text/css"/>
+</head> 
+<body>
+    <div class="container">
+        <center><h1>Web Clock</h1></center>
+        <div id="time_card"></div>
+        <div id="bl"></div>
+        <div id='bl_p'></div>
+    </div>
+</body>
+</html>
