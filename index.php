c299c187bed29edcc7afe9f4be7576b9cc14f2e7
Joseph Zhang <tweejjhi4016@163.com>
2022-01-03 13:25:49 +0800

    1

--- index.php
+++ index.php
@@ -28,7 +28,9 @@
     <title>Webclock</title>
     <script src="js/jquery-3.4.1.min.js" type="text/javascript"></script>
     <script src="js/webclock.js" type="text/javascript"></script>
+    <script src="js/cale.js" type="text/javascript"></script>
     <link rel="stylesheet" href="css/main.css" type="text/css"/>
+    <link rel="stylesheet" href="css/cale.css" type="text/css"/>
 </head> 
 <body>
     <div class="container">
@@ -37,5 +39,61 @@
         <div id="bl"></div>
         <div id='bl_p'></div>
     </div>
+<?php
+	date_default_timezone_set("PRC");
+    
+    $time = getdate();
+    $mday = $time["mday"];
+    $mon = $time["mon"];
+    $year = $time["year"];
+    
+    if($mon==4||$mon==6||$mon==9||$mon==11){
+        $day = 30;
+    }elseif($mon==2){
+        if(($year%4==0&&$year%100!=0)||$year%400==0){
+            $day = 29;
+        }else{
+            $day = 28;
+        }
+    }else{
+        $day = 31;
+    }
+    
+    $w = getdate(mktime(0,0,0,$mon,1,$year))["wday"];
+    
+    $date = function($day,$w,$year,$mon){
+        echo "<div class=\"cale\"><center><b>".$year."年".$mon."月</b><table border='0.5'>";
+        echo "<tr><th>日</th><th>一</th><th>二</th><th>三</th><th>四</th><th>五</th><th>六</th></tr>";
+        $arr = array();
+        for($i=1;$i<=$day;$i++){
+            array_push($arr,$i);
+        }
+        if($w>=1&&$w<=6){
+            for($m=1;$m<=$w;$m++){
+                array_unshift($arr,"");
+            }
+        }
+        $n=0;
+        for($j=1;$j<=count($arr);$j++){
+            $n++;
+            if($n==1) echo "<tr>";
+            global $mday;
+            if($mday==$arr[$j-1]){
+                echo "<td width='80px' style='background-color: #90a0ee;'>".$arr[$j-1]."</td>";
+            }else{
+                echo "<td width='80px'>".$arr[$j-1]."</td>";
+            }
+            
+            if($n==7){
+                echo "</tr>";
+                $n=0;
+            }
+        }
+        if($n!=7)echo "</tr>";
+        
+        echo "</table></center></div>";
+    };
+    $date($day,$w,$year,$mon);
+?>
 </body>
 </html>
